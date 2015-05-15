2a3d1267554eace75d80eec2307eafe66b3cbd57
vik denic <vik.denic@gmail.com>
2015-05-15 15:54:03 -0500

    Initial Commit

---
+++ BaeWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaeWatch
+//
+//  Created by Vik Denic on 5/15/15.
+//  Copyright (c) 2015 nektar labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
