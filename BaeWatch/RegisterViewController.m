2a09b95253f1375ac8bddcaa5df4fe1b6075dc45
vik denic <vik.denic@gmail.com>
2015-05-15 22:55:58 -0500

    register / login to parse via fb
    using pffacebookutilsv4

--- BaeWatch/RegisterViewController.m
+++ BaeWatch/RegisterViewController.m
@@ -11,7 +11,6 @@
 @interface RegisterViewController ()
 
 @property (strong, nonatomic) IBOutlet UITextField *usernameTextField;
-
 @property (strong, nonatomic) IBOutlet UITextField *passwordTextField;
 
 @end
@@ -21,7 +20,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (IBAction)onRegisterTapped:(UIButton *)sender
@@ -30,5 +28,74 @@
         [self dismissViewControllerAnimated:YES completion:nil];
     }];
 }
+
+- (IBAction)onFBLoginTapped:(FBSDKLoginButton *)sender
+{
+    [self _loginWithFacebook];
+}
+
+- (void)_loginWithFacebook
+{
+    // Set permissions required from the facebook user account
+    NSArray *permissionsArray = @[@"public_profile", @"user_friends", @"email"];
+
+    // Login PFUser using Facebook
+    [PFFacebookUtils logInInBackgroundWithReadPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
+        if (!user)
+        {
+            NSLog(@"Uh oh. The user cancelled the Facebook login.");
+        }
+        else if (user.isNew) {
+            NSLog(@"User signed up and logged in through Facebook!");
+        }
+        else
+        {
+            NSLog(@"User logged in through Facebook!");
+            [self _loadData];
+        }
+    }];
+}
+
+- (void)_loadData
+{
+    // ...
+    FBSDKGraphRequest *request = [[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:nil];
+
+    [request startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+        if (!error) {
+            // result is a dictionary with the user's Facebook data
+            NSDictionary *userData = (NSDictionary *)result;
+
+            NSString *facebookID = userData[@"id"];
+            NSString *name = userData[@"name"];
+            NSString *location = userData[@"location"][@"name"];
+
+            NSLog(@"%@ %@ %@", facebookID, name, location);
+
+            PFUser *currentUser = [PFUser currentUser];
+            [currentUser setObject:name forKey:@"name"];
+            [currentUser saveInBackground];
+
+            NSURL *pictureURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?type=large&return_ssl_resources=1", facebookID]];
+
+            NSURLRequest *urlRequest = [NSURLRequest requestWithURL:pictureURL];
+
+            // Now add the data to the UI elements
+            // Run network request asynchronously
+            [NSURLConnection sendAsynchronousRequest:urlRequest
+                                               queue:[NSOperationQueue mainQueue]
+                                   completionHandler:
+             ^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                 if (connectionError == nil && data != nil) {
+
+                     PFFile *file = [PFFile fileWithData:data];
+                     [currentUser setObject:file forKey:@"profileImage"];
+                     [currentUser saveInBackground];
+                 }
+             }];
+        }
+    }];
+}
+
 
 @end
