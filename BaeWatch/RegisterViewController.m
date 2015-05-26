7c1c0ff17796752949469766b03b24a9b98f226c
vik denic <vik.denic@gmail.com>
2015-05-25 23:46:17 -0500

    clean up some code

--- BaeWatch/RegisterViewController.m
+++ BaeWatch/RegisterViewController.m
@@ -20,8 +20,6 @@
 NSString *const kSegueIDRegisterToNameEntry = @"RegisterToNameEntrySegue";
 
 @implementation RegisterViewController
-- (IBAction)registerButton:(UIButton *)sender {
-}
 
 - (void)viewDidLoad
 {
@@ -70,48 +68,7 @@ NSString *const kSegueIDRegisterToNameEntry = @"RegisterToNameEntrySegue";
     [self _loginWithFacebook];
 }
 
-//Executes login or registration with Facebook
-//Asks for permissions to the user's info
-//Will log them in, create the account AND log them in, or tell you it was cancelled
-- (void)_loginWithFacebook
-{
-    // Set permissions required from the facebook user account
-    NSArray *permissionsArray = @[@"public_profile", @"user_friends", @"email"];
-
-    // Login PFUser using Facebook
-    [PFFacebookUtils logInInBackgroundWithReadPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
-        if (error == nil)
-        {
-            if (!user)
-            {
-                NSLog(@"Uh oh. The user cancelled the Facebook login.");
-            }
-            //When a new user is registered through Facebook
-            ////Set their unique fbid to the User class
-            ////Create a Profile for them, with their info from FB graph set
-            ////And set the share Profile singleton to this
-            else if (user.isNew)
-            {
-                NSLog(@"User signed up and logged in through Facebook!");
-                [FBManager setUsersFbIdWithBlock:^(NSError *error) {
-                    [self createAndSaveProfile];
-                    [self dismissViewControllerAnimated:YES completion:nil];
-                }];
-            }
-            else
-            {
-                NSLog(@"User logged in through Facebook!");
-                [self setProfileSingleton];
-                [self dismissViewControllerAnimated:YES completion:nil];
-            }
-        }
-        else
-        {
-            [VZAlert showAlertWithTitle:@"Oops" message:error.localizedDescription viewController:self];
-        }
-    }];
-}
-
+//Toggles whether the user is creating a new account or logging in
 - (IBAction)onSegmentTapped:(UISegmentedControl *)sender
 {
     if (sender.selectedSegmentIndex == 0)
@@ -151,6 +108,48 @@ NSString *const kSegueIDRegisterToNameEntry = @"RegisterToNameEntrySegue";
 
         Profile *currentProfile = (Profile *) profile;
         [[UniversalProfile sharedInstance] setProfile:currentProfile];
+    }];
+}
+
+//Executes login or registration with Facebook
+//Asks for permissions to the user's info
+//Will log them in, create the account AND log them in, or tell you it was cancelled
+- (void)_loginWithFacebook
+{
+    // Set permissions required from the facebook user account
+    NSArray *permissionsArray = @[@"public_profile", @"user_friends", @"email"];
+
+    // Login PFUser using Facebook
+    [PFFacebookUtils logInInBackgroundWithReadPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
+        if (error == nil)
+        {
+            if (!user)
+            {
+                NSLog(@"Uh oh. The user cancelled the Facebook login.");
+            }
+            //When a new user is registered through Facebook
+            ////Set their unique fbid to the User class
+            ////Create a Profile for them, with their info from FB graph set
+            ////And set the share Profile singleton to this
+            else if (user.isNew)
+            {
+                NSLog(@"User signed up and logged in through Facebook!");
+                [FBManager setUsersFbIdWithBlock:^(NSError *error) {
+                    [self createAndSaveProfile];
+                    [self dismissViewControllerAnimated:YES completion:nil];
+                }];
+            }
+            else
+            {
+                NSLog(@"User logged in through Facebook!");
+                [self setProfileSingleton];
+                [self dismissViewControllerAnimated:YES completion:nil];
+            }
+        }
+        else
+        {
+            [VZAlert showAlertWithTitle:@"Oops" message:error.localizedDescription viewController:self];
+        }
     }];
 }
 
