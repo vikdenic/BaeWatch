ae5857fbf30bfc205af7a457c9b3050950188f74
vik denic <vik.denic@gmail.com>
2015-05-29 04:09:35 -0500

    further fasten cloud code

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -2,19 +2,20 @@
 
 Parse.Cloud.beforeSave("Activity", function(request, response) {
 	    var Activity = Parse.Object.extend("Activity");
-	    // Find every Profile with same entityID
+	    // Find any follow activity w/ the same to and from profiles
 	    var query = new Parse.Query(Activity);
+	    query.equalTo("type", "Follow");
 	    query.equalTo("fromProfile", request.object.get("fromProfile"));
 	    query.equalTo("toProfile", request.object.get("toProfile"));
 		
-	query.find().then(function(results) {
-		if (results.length > 0)
-		{
-	  		response.error('duplicate activity prevented');
-		}
-		else
-		{
-	  		response.success();			
-		}
-	});
+		query.find().then(function(results) {
+			if (results.length > 0)
+			{
+		  		response.error('duplicate activity prevented');
+			}
+			else
+			{
+		  		response.success();			
+			}
+		});
 });
