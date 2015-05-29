b471a6a20180025a220567f8ccb6631a0e296b52
vik denic <vik.denic@gmail.com>
2015-05-29 04:01:48 -0500

    write cloudcode to prevent duplicate activities

---
+++ parse/cloud/main.js
@@ -0,0 +1,20 @@
+
+
+Parse.Cloud.beforeSave("Activity", function(request, response) {
+	    var Activity = Parse.Object.extend("Activity");
+	    // Find every Profile with same entityID
+	    var query = new Parse.Query(Activity);
+	    // query.equalTo("fromProfile", request.object.get("fromProfile"));
+	    query.equalTo("toProfile", request.object.get("toProfile"));
+		
+	query.find().then(function(results) {
+		if (results.length > 0)
+		{
+	  		response.error('duplicate activity prevented');
+		}
+		else
+		{
+	  		response.success();			
+		}
+	});
+});
